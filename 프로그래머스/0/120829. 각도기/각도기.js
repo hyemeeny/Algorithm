8b30fc9762608231f2aab0f49be986ebb6ed7d30
HYEMIN JI <144791802+hyemeeny@users.noreply.github.com>
2024-08-20 13:31:18 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -4,6 +4,6 @@ function solution(angle) {
     if(90 < angle && angle < 180) return 3
     if(180 === angle) return 4
     
-    //return [1, 90, 91, 180].filter(x => x<=angle).length;
+    // return [1, 90, 91, 180].filter(x => console.log(x<=angle)).length;
     //return angle < 90 ? 1 : 90 === angle ? 2 : angle < 180 ? 3 : 4 
 }
