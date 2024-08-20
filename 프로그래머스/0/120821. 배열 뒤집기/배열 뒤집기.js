75606a69749128e44591bc65ab41a6e216d70683
HYEMIN JI <144791802+hyemeeny@users.noreply.github.com>
2024-08-20 13:02:08 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -0,0 +1,14 @@
+function solution(num_list) {
+    return num_list.reverse();
+    
+//     const answer = [];
+//     for(let i=1; i<=num_list.length; i++) {
+//         answer.push(num_list[num_list.length-i]);
+//     }
+    
+//     return answer;
+    
+// const answer = [];
+// num_list.forEach(i => answer.unshift(i))
+// return answer;
+}
