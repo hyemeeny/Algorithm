1d3fb1b6724072b82e5a27b487762a4e221d7e44
HYEMIN JI <144791802+hyemeeny@users.noreply.github.com>
2024-09-10 22:03:43 +0900

    [level 0] Title: 개미 군단, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,8 +1,8 @@
 function solution(hp) {
-  const first = Math.floor(hp / 5);
-  const second = Math.floor((hp - first * 5) / 3);
-  const third = hp - first * 5 - second * 3;
-  return first + second + third;
+    const first = Math.floor(hp / 5);
+    const second = Math.floor((hp - first * 5) / 3);
+    const third = hp - first * 5 - second * 3;
+    return first + second + third;
     
   // return Math.floor(hp/5)+Math.floor((hp%5)/3)+(hp%5)%3;  
 }
