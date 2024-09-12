94450a0ddfa10597caffbee2b1657fb7050e3bd8
HYEMIN JI <144791802+hyemeeny@users.noreply.github.com>
2024-09-12 23:21:35 +0900

    [level 0] Title: 공 던지기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.js
@@ -0,0 +1,10 @@
+function solution(numbers, k) {
+    const index = 2*(k-1) % numbers.length;
+    return numbers[index];
+}
+
+// k번째 공 던지는 사람의 배열의 인덱스 => 2 * (k - 1)
+// 배열의 인덱스는 0부터 시작이므로 -1 해줘야함.
+
+// % numbers.length 를 통해 배열의 길이를 초과할 경우 처음으로 돌아가게 됨
+// k번째 공 던진 사람의 위치 찾는 식
