cacb632a23ad98d9448bd96abc507e9017f39803
HYEMIN JI <144791802+hyemeeny@users.noreply.github.com>
2024-09-12 22:56:39 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,3 +1,4 @@
+// splice 방식 => 배열에서 요소를 추가, 제거 또는 교체하는 작업을 수행한다. 이때 원본 배열이 수정된다.
 function solution(num_list, n) {
     const answer = [];
     const rows = num_list.length/n;
@@ -8,3 +9,15 @@ function solution(num_list, n) {
     
     return answer;
 }
+
+// slice 방식 => 배열의 일부분을 추출하여 새로운 배열을 반환한다. 원본 배열은 변경되지 않는다.
+// function solution(num_list, n) {
+//     const answer = [];
+//     const rows = num_list.length;
+    
+//     for(let i=0; i<rows; i+=n) {
+//         answer.push(num_list.slice(i, i+n))
+//     }
+    
+//     return answer;
+// }
