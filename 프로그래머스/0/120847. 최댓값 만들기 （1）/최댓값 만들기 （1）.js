93919bd57c56c6a8f1abc9b49315d1234a6f4195
HYEMIN JI <144791802+hyemeeny@users.noreply.github.com>
2024-09-23 23:19:18 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,5 +1,4 @@
 function solution(numbers) {
-    const first = numbers.sort((a,b) => b-a)[0];
-    const second = numbers.sort((a,b) => b-a)[1];
-    return first*second;
+    numbers.sort((a,b) => b-a);
+    return numbers[0]*numbers[1];
 }
