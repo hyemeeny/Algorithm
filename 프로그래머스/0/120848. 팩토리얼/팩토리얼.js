6249265b99463461c1393b75366aa51cbe2ab4f6
HYEMIN JI <144791802+hyemeeny@users.noreply.github.com>
2024-09-23 23:45:14 +0900

    [level 0] Title: 팩토리얼, Time: 1.94 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.js
@@ -9,3 +9,13 @@ function solution(n) {
     
     return answer;
 }
+
+// function solution(n) {
+//   let factorial = 1;
+//   let i = 0;
+//   while (factorial <= n) {
+//     i++;
+//     factorial *= i;
+//   }
+//   return i - 1;
+// }
