f43183590564dbeaaf41e3c3fd09286c8a7a2c52
HYEMIN JI <144791802+hyemeeny@users.noreply.github.com>
2024-09-27 00:56:07 +0900

    [level 0] Title: 소인수분해, Time: 0.26 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    const answer = [];
+    
+    for(let i=2; i<=n; i++) {
+        while(n%i === 0) {
+            n /= i;
+            answer.push(i);
+        }
+    }
+    
+    return [... new Set(answer)];
+}
