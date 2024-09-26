ddb8098a49ee725f196a38181372515eb83a4c3e
HYEMIN JI <144791802+hyemeeny@users.noreply.github.com>
2024-09-27 01:04:37 +0900

    [level 0] Title: 소인수분해, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -10,3 +10,8 @@ function solution(n) {
     
     return [... new Set(answer)];
 }
+
+// 나누어 떨어져야 하니까 2부터 시작
+// while문으로 n%i === 0 조건이 false일 때 까지 반복하고 종료 (true면 계속 반복)
+// 6(n) = 12(n)/2(i)를 해서 6(n)은 다시 반복되고 몫 i는 answer 배열에 담김
+// set으로 중복 제거
