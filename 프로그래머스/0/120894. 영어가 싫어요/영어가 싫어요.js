b8acd2768df8ddd40a072fbc0397262c12e94305
HYEMIN JI <144791802+hyemeeny@users.noreply.github.com>
2024-10-06 20:52:46 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -2,7 +2,7 @@ function solution(numbers) {
     const numberStr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     
     numberStr.forEach((str, i) => {
-        // replaceAll로 numberStr 모든 문자열을 index로 치환 
+        // replaceAll로 numbers 모든 문자열을 index로 치환 
         numbers = numbers.replaceAll(str, i);
     })
     
