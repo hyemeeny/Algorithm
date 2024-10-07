cb4d89e3bc5bbfaf50f6dabcdf00b41add39ffc7
HYEMIN JI <144791802+hyemeeny@users.noreply.github.com>
2024-10-07 21:53:26 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -2,6 +2,8 @@ function solution(s) {
     let answer = '';
     const arr = [...s].sort()
     arr.forEach((str, i) => {
+        // arr 배열에서 현재 문자열 str 앞에 문자열(arr[i+1])과 다르고 
+        // 뒤에 문자열(arr[i-1])과도 다를 경우 answer에 넣어준다
         if(str !== arr[i+1] && str !== arr[i-1]) {
             answer += str
         }
