f10053f811d73a1c2523b5c1b63b502f78ce6b51
HYEMIN JI <144791802+hyemeeny@users.noreply.github.com>
2024-10-14 17:57:10 +0900

    [level 0] Title: OX퀴즈, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
@@ -2,8 +2,11 @@ function solution(quiz) {
     const answer = [];
     
     quiz.map((v, i) => {
-        let q = v.split(" ")
-        let calc = q[1] === '+' ? +q[0] + +q[2] : +q[0] - +q[2]
+        // map으로 배열안에 수식들을 나눠주고 split을 이용해 공백 제거하고 전부 문자열로 변환
+        const q = v.split(" ")
+        // 1번 인덱스가 문자열 +이면 0번 인덱스와 2번 인덱스를 숫자로 변환 후 플러스 아니면 마이너스 
+        const calc = q[1] === '+' ? +q[0] + +q[2] : +q[0] - +q[2]
+        // 4번 인덱스가 calc에서 연산한 답과 같으면 "0" 아니면 "X"
         answer.push(+q[4] === calc ? 'O' : 'X')
     })
     
