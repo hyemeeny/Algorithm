ceeaa7874ce5d9a85467081f3031b1115bb69c6a
HYEMIN JI <144791802+hyemeeny@users.noreply.github.com>
2024-10-14 18:00:30 +0900

    [level 0] Title: OX퀴즈, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
@@ -1,7 +1,7 @@
 function solution(quiz) {
     const answer = [];
     
-    quiz.map((v, i) => {
+    quiz.map((v) => {
         // map으로 배열안에 수식들을 나눠주고 split을 이용해 공백 제거하고 전부 문자열로 변환
         const q = v.split(" ")
         // 1번 인덱스가 문자열 +이면 0번 인덱스와 2번 인덱스를 숫자로 변환 후 플러스 아니면 마이너스 
