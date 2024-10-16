1c50b0adbf62fdc1cd150952d5fce24e0ddf6f5b
HYEMIN JI <144791802+hyemeeny@users.noreply.github.com>
2024-10-17 00:44:48 +0900

    [level 0] Title: 배열 만들기 5, Time: 14.93 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
@@ -5,10 +5,7 @@ function solution(intStrs, k, s, l) {
     for(let i of intStrs) {
         // i를 split으로 잘라주고 splice로 s번째부터 l까지 temp에 넣어줌 
         temp = Number(i.split('').splice(s,l).join(''));
-        
-        if(temp > k) {
-            answer.push(temp)
-        }
+        if(temp > k) answer.push(temp)
     }
     return answer;
 }
