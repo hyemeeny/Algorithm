ddf7a9b311bcb6ae4193c6f2a403471bbf61c636
JIHYEMIN <144791802+hyemeeny@users.noreply.github.com>
2026-03-18 19:04:36 +0900

    [level 0] Title: 배열 만들기 3, Time: 30.05 ms, Memory: 41.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -0,0 +1,8 @@
+function solution(arr, intervals) {
+    const answer = [
+        ...arr.slice(intervals[0][0], intervals[0][1]+1),
+        ...arr.slice(intervals[1][0], intervals[1][1]+1)
+    ];
+                  
+    return answer;
+}
