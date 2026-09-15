3a76438ac30683c4686d08f15c6bd65ec9c1f7a9
JIHYEMIN <144791802+hyemeeny@users.noreply.github.com>
2026-09-15 19:19:26 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 33.62 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,17 +1,17 @@
 function solution(participant, completion) {
-    const emptyMap = new Map();
+    const map = new Map();
     
     const mapFunction = (arr, delta) => {
         arr.forEach((v) => {
-            const current = emptyMap.get(v) || 0;
-            emptyMap.set(v, current+delta);
+            const current = map.get(v) || 0;
+            map.set(v, current+delta);
         })
     }
     
     mapFunction(participant, 1)
     mapFunction(completion, -1)
     
-    for(const item of emptyMap) {
-        if(item[1] > 0) return item[0]
+    for(const [k, v] of map) {
+        if(v > 0) return k
     }
 }
