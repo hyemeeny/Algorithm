5872467704fdd5ab741efde64f7a2562130a90b8
JIHYEMIN <144791802+hyemeeny@users.noreply.github.com>
2026-09-22 20:12:55 +0900

    [level 1] Title: 폰켓몬, Time: 0.62 ms, Memory: 43.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,11 +1,7 @@
 function solution(nums) {
-    var answer = 0;
     const setNums = new Set(nums);
-    const maxNums = nums.length/2;
-    const unique = [...setNums]
-    console.log('중복없앤포켓몬배열', [...setNums])
-    console.log('받을수있는포켓몬최대', maxNums)
+    const maxPick = nums.length/2;
+    const unique = [...setNums];
     
-    console.log(Math.min(maxNums, unique.length))
-    return Math.min(maxNums, unique.length);
+    return Math.min(maxPick, unique.length);
 }
